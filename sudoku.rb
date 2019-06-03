afebc90b0ecd66a74b818c92fe53327576747ae8
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-06-03 18:08:52 -0300

    refill content and changes in evaluate functions.

--- sudoku.rb
+++ sudoku.rb
@@ -1,66 +1,71 @@
 class Alga
 	SIZE_SUDOKU = 4
-	attr_accessor(:oxygen, :content)	
-
+	OXYGEN_ONE_VALUE = 2.08
+	
+	attr_accessor(:oxygen, :content, :new_content)	
 
 	def initialize
 		@content = fill_content
+		@new_content = @content
+		@new_content = refill_content(@new_content)
 		@oxygen = evaluate_oxygen
 	end
 
 	def fill_content
 		filled_array = []
 
-		SIZE_SUDOKU.times { |i| filled_array.push(4.times.map { rand(1..4) }) }		
+		SIZE_SUDOKU.times { |i| filled_array.push(SIZE_SUDOKU.times.map { rand(1..SIZE_SUDOKU) }) }		
 
 		return filled_array
 	end
 
-	def refill_content(line)
-
+	def refill_content(old_content)
+		old_content.each.map { |line| line.each.map { |v| line.count(v) > 1 ? nil : v } }
+		old_content.transpose.each.map { |column| column.each.map { |v| column.count(v) > 1 ? nil : v } }
 	end
 
 	def evaluate_oxygen
 		oxygen_produced = 0
-		squares = generate_squares(@content)
-		
-		p @content
-		p squares
-
-		@content.each { |line| evaluate_line(line) ? oxygen_produced += 8.3 : 0 }
-		@content.transpose.each { |column| evaluate_column(column) ? oxygen_produced += 8.3 : 0 }
-		
+		squares = generate_squares(@new_content)
+			
+		@new_content.each { |line| oxygen_produced += evaluate_line(line) }
+		@new_content.transpose.each { |column| oxygen_produced += evaluate_column(column) }
+		squares.each {|square| oxygen_produced += evaluate_square(square) }
 
 		return oxygen_produced
 	end
 
 	def generate_squares(content)
 		squares = []
-				
-
-
+		
 		for i in (0..content.size-1).step(content.size/2)
 			square_1 = (content[i][0..content.size/2-1] << content[i+1][0..content.size/2-1]).flatten
 			square_2 = (content[i][content.size/2..content.size/2+1] << content[i+1][content.size/2..content.size/2+1]).flatten
 
-			squares << square_1
-			squares << square_2
+			squares << square_1 << square_2
 		end
 		
 		return squares
 	end
 
 	def evaluate_line(line)
-		return line.uniq.size.eql?(SIZE_SUDOKU)
+		return line.compact.uniq.size * OXYGEN_ONE_VALUE
 	end
 
 	def evaluate_column(column)
-		return column.uniq.size.eql?(SIZE_SUDOKU)
+		return column.compact.uniq.size * OXYGEN_ONE_VALUE
 	end
 
 	def evaluate_square(square)
-		return square.uniq.size.eql?(SIZE_SUDOKU)
+		return square.compact.uniq.size * OXYGEN_ONE_VALUE
+	end
+
+	def binary_division
+		child = @new_content
+
+		return child
 	end
 end
 
 algas = Array.new(1){Alga.new}
+p algas[0].oxygen
