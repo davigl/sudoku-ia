ea2da08ecbf25c5282b21b17ec9a8ee1a9507c56
Davi <davi.guimaraesleite@gmail.com>
2019-06-13 02:24:58 -0300

    alga model comment.

--- alga.rb
+++ alga.rb
@@ -1,8 +1,19 @@
 class Alga
+
+	# Constantes
+
 	SIZE_SUDOKU = 4
 	OXYGEN_ONE_VALUE = 100/((SIZE_SUDOKU ** 2) * 3).to_f
 	
+	# Métodos de Get (alga.oxygen, alga.content, alga.new_content)
+	# Varíaveis: oxygen:float; :content:array; new_content:array
+
 	attr_accessor(:oxygen, :content, :new_content)	
+
+	# Construtor do Objeto Alga.
+	#
+	# Parametros: sudoku:array
+	# Atributos: content:array ; new_content; oxygen:float
 
 	def initialize(sudoku)
 		@content = fill_content(sudoku)
@@ -10,6 +21,12 @@ class Alga
 		@new_content = refill_content(@new_content)
 		@oxygen = evaluate_oxygen
 	end
+
+	# Método que recebe um sudoku pré-preenchido e termina de preenchê-lo
+	# com valores randomicos (1 até o tamanho do sudoku).
+	# 
+	# Parâmetros: sudoku:array
+	# Retorno: filled_array:array
 
 	def fill_content(sudoku)
 		filled_array = sudoku
@@ -25,11 +42,20 @@ class Alga
 		return filled_array
 	end
 
+	# Método que trata a alga, permancendo somente os valores corretos na matriz.
+	# 
+	# Parâmetros: old_content:array
+
 	def refill_content(old_content)
 		old_content.each.map do |line| 
 			line.each_with_index { |v, i| line.count(v) > 1 ? worst_of_line(old_content, line, v).each { |k, v| line[k.to_s.to_i] = nil } : v }
 		end
 	end
+
+	# Método auxiliar que retorna os piores indices para serem removidos em caso de valores clonados em uma linha.
+	# 
+	# Parâmetros: content:array; line:array; value:string
+	# Retorno: worst_indexes:hash
 
 	def worst_of_line(content, line, value)
 		temp = content.transpose
@@ -41,15 +67,13 @@ class Alga
 		worst_indexes.sort_by {|k, v| v}.reverse.first(line_count).to_h
 	end
 
-	def worst_of_column(content, line, value)
-		temp = content.transpose
-		worst_indexes = {}
-		line_count = line.count(value) - 1
+	# Not implemented yet
 
-		temp.each_with_index { |column, i| worst_indexes[i.to_s.to_sym] = column.count(value) }
+	def worst_of_column(content, column); end
 
-		worst_indexes.sort_by {|k, v| v}.reverse.first(line_count).to_h
-	end
+	# Método para avaliar a produção de oxigênio da Alga.
+	#
+	# Retorno: oxygen_produced:float
 
 	def evaluate_oxygen
 		oxygen_produced = 0
@@ -61,6 +85,11 @@ class Alga
 
 		return oxygen_produced
 	end
+
+	# Método auxiliar que retorna os quadrantes da matriz.
+	#
+	# Parâmetros: content:array
+	# Retorno: squares:array
 
 	def generate_squares(content)
 		squares = []
@@ -75,17 +104,40 @@ class Alga
 		return squares
 	end
 
+	# Método auxiliar que avalia a produção de oxigênio de uma linha.
+	#
+	# Parâmetros: line:array
+	# Retorno: line_oxygen_produced:float 
+
 	def evaluate_line(line)
-		return line.compact.uniq.size * OXYGEN_ONE_VALUE
+		line_oxygen_produced = line.compact.uniq.size * OXYGEN_ONE_VALUE
+
+		return line_oxygen_produced
 	end
+
+	# Método auxiliar que avalia a produção de oxigênio de uma coluna.
+	#
+	# Parâmetros: column:array
+	# Retorno: column_oxygen_produced:float 
 
 	def evaluate_column(column)
-		return column.compact.uniq.size * OXYGEN_ONE_VALUE
+		column_oxygen_produced = column.compact.uniq.size * OXYGEN_ONE_VALUE
+
+		return column_oxygen_produced
 	end
 
+	# Método auxiliar que avalia a produção de oxigênio de um quadrante.
+	#
+	# Parâmetros: square:array
+	# Retorno: square_oxygen_produced:float 
+
 	def evaluate_square(square)
-		return square.compact.uniq.size * OXYGEN_ONE_VALUE
+		square_oxygen_produced = square.compact.uniq.size * OXYGEN_ONE_VALUE
+
+		return square_oxygen_produced
 	end
+
+	# Not implemented yet.
 
 	def binary_division
 		child = @new_content
